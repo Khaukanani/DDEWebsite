1bd3c536f27de3228e8ab33b003f74f57501acff
Khaukanani <klawrence026@gmail.com>
2019-06-25 02:23:01 -0700

    Adding hms files

---
+++ DDA/footer.php
@@ -0,0 +1,42 @@
+  <!-------------Footer markups--------------->
+       
+       <footer class="footer">
+           <div class="container">
+               <div class="row">
+                   <div class="col-sm-12 text-center">
+                       <div class="social-icon">
+                         <a href="https://www.facebook.com/pages/category/Company/Deshun-Deysel-Associates-150726751673784/"><i class="fa fa-facebook wow fadeInLeft" data-wow-duration=".3s" data-wow-delay=".3s" ></i></a>  
+                         <a href="https://twitter.com/DeyselAssoc"><i class="fa fa-twitter wow fadeInLeft" data-wow-duration=".6s" data-wow-delay=".6s"></i></a>     
+                         <a href="https://www.linkedin.com/company/deshun-deysel-&-associates"><i class="fa fa-linkedin wow fadeInLeft" data-wow-duration="1.8s" data-wow-delay="1.8s" ></i></a>  
+                                                
+                       </div> <br>
+                       <div class="copyright">
+                           <p class="footerlogo">
+                               &copy; <img src="<?php bloginfo('template_url'); ?>/img/logo.png">
+                           </p>
+                           <div class="bordernow"></div>
+                       </div>
+                                            
+                                                                                      
+                    </div>
+                   
+                   
+                   
+               </div>
+           </div>
+       
+       </footer>
+       <?php wp_footer(); ?>
+
+         <script src="<?php bloginfo('template_url'); ?>/js/jquery.ripples-min.js" type="text/javascript"></script>  
+         <script src="<?php bloginfo('template_url'); ?>/js/typed.min.js" type="text/javascript"></script>
+         <script src="<?php bloginfo('template_url'); ?>/js/jquery.waypoints.min.js" type="text/javascript"></script>
+         <script src="<?php bloginfo('template_url'); ?>/js/jquery.counterup.min.js" type="text/javascript"></script>           
+         <script src="<?php bloginfo('template_url'); ?>/js/main.js" type="text/javascript"></script>
+         <script src="<?php bloginfo('template_url'); ?>/js/smoothscroll.js" type="text/javascript"></script>
+         <script src="<?php bloginfo('template_url'); ?>/js/jquery.easing.1.3.js" type="text/javascript"></script>
+          <script src="<?php bloginfo('template_url'); ?>/js/wow.min.js" type="text/javascript"></script>
+
+          <script src="<?php bloginfo('template_url'); ?>/lib/jquery/jquery-migrate.min.js"></script>          
+     </body>
+ </html>
---
+++ DDA/index.php
@@ -0,0 +1,283 @@
+ <?php get_header(); ?>
+          <!---------------Slider section starts------------->
+          <section class="slider text-center" id="slider">              
+              <div class="slider-overlay">
+                  <div class="slider-content">
+                      <div class="icons">
+                          <i class="fa fa-cogs"></i>
+                          <i class="fa fa-line-chart"></i>
+                          <i class="fa fa-trophy"></i>
+                      </div>
+                      <br>
+                      <div class="text"></div>
+                      <div class="cta-div">
+                          <a href="#contact-form" class="btn1">Get Started!</a>
+                          <a href="#video" class="btn2"> INTRO VIDEO!</a>
+                      </div>
+                      <br><br>
+                      <div class="social-networks">
+                          <a href="https://www.facebook.com/pages/category/Company/Deshun-Deysel-Associates-150726751673784/" class="fa fa-facebook"></a>
+                          <a href="https://twitter.com/DeyselAssoc" class="fa fa-twitter"></a>
+                          <a href="https://www.linkedin.com/company/deshun-deysel-&-associates" class="fa fa-linkedin"></a>
+                      </div>                      
+                  </div>                  
+              </div>       
+          </section>
+         
+         <!---------------Slider section ends------------->
+         
+         <!---------------Services Markups------------->
+          
+          <br> 
+          <section class="service-area" id="services">
+              <div class="container">
+                  <div class="row">
+                      <div class="col-xs-12">
+                          <div class="section-title text-center wow zoomIn" data-wow-duration=".5s" data-wow-delay=".5s">
+                              <h2><b>SERVICES</b></h2>
+                              <p>
+                                  Driving Agile Performance Development
+                              </p>
+                          </div>
+                      </div>                      
+                  </div><br><br>
+                  <div class="row wow bounceInUp"  data-wow-duration="1.2s" data-wow-delay="1.2s">
+                      <div class="col-md-4 col-sm-6 col-xs-12">
+                          <div class="service-wrap text-center">
+                              <div class="service-icon">
+                                  <i class="fa fa-bar-chart"></i>
+                              </div>
+                              <h3>Surveys</h3>
+                              <p>
+                                  Provides unique perspectives into Emotional, Cultural and People Intelligence, to assist organizations in Driving Agile Performance Development at the rate of change.
+                              </p>
+                          </div>
+                          
+                      </div>
+                         
+                     <div class="col-md-4 col-sm-6 col-xs-12">
+                          <div class="service-wrap text-center">
+                              <div class="service-icon">
+                                  <i class="fa fa-stethoscope"></i>
+                              </div>
+                               <h3>Diagnostics</h3>
+                              <p>
+                                  The PPT&trade; Diagnostic is a self-reflective tool which facilitates on-going check-ins <br> to assist users in correcting performance gaps in real-time. 
+                              </p>
+                          </div>
+                    </div> 
+                         
+                         <div class="col-md-4 col-sm-6 col-xs-12">
+                          <div class="service-wrap text-center">
+                              <div class="service-icon">
+                                  <i class="fa fa-users"></i>
+                              </div>
+                               <h3>Business and Career Coaching</h3>
+                              <p>
+                                  Our global pool of coaches have a proven track record in helping clients reach high performance levels in alignment with <br> their career and business objectives.
+                              </p>
+                          </div>
+                          
+                      </div>
+                         
+                         <div class="col-md-4 col-sm-6 col-xs-12">
+                          <div class="service-wrap text-center">
+                              <div class="service-icon">
+                                  <i class="fa fa-trophy"></i>
+                              </div>
+                              <h3>Masterclass</h3>
+                              <p>
+                                  Our 1 Day Masterclass equips business leaders with the tools to Drive High Performance in real-time
+                              </p>
+                          </div>
+                          
+                      </div>
+                         
+                                          
+                         
+                         <div class="col-md-4 col-sm-6 col-xs-12">
+                          <div class="service-wrap text-center">
+                              <div class="service-icon">
+                                  <i class="fa fa-microphone"></i>
+                              </div>
+                              <h3>Talks & Presentations</h3>
+                              <p>
+                                  With over 20 years of professional speaking in 17 countries on 5 continents, our speakers are well placed to bring inspiration into new <br>ways of work, as well as global insight to<br> Drive Performance Development.
+                              </p>
+                          </div>
+                          
+                      </div> 
+
+                      <br>
+                         <div class="col-md-4 col-sm-6 col-xs-12">
+                          <div class="service-wrap text-center">
+                              <div class="service-icon">
+                                  <i class="fa fa-balance-scale"></i>
+                              </div>
+                              <h3>Consulting and Support</h3>
+                              <p>
+                                  In partnership with our clients we co-design customized Performance Development journeys to suit their  specific needs.
+                              </p>
+                          </div>
+                          
+                      </div>                    
+                      
+                  </div>                 
+                  
+              </div>             
+                          
+          </section>
+        
+       <!------Services Markups end-------->
+
+       
+       <!--==========================
+      About Us Section
+    ============================-->
+    <section id="about">
+      <div class="container">
+
+        <header class="section-header">
+          <h3>About Us</h3>
+          <p>
+            Aspiration, Self Mastery, Competence, Environment, Opportunity and Delivery are 6 of the major factors which drives performance in today's workplace. It also happens to be the Areas of Effectiveness which makes high altitude expeditions successful. By combining cutting edge research from organizations and big mountains, worldwide, DDA has formulated a digital platform called the Peak Performance Toolkit™ to equip teams with tools to Drive Agile Performance Development for career and business success. 
+          </p>
+        </header>
+
+        <div class="row about-cols">
+
+          <div class="col-md-3 wow fadeInUp">
+            <div class="about-col">
+              <div class="img">
+                <img src="<?php bloginfo('template_url'); ?>/img/vision2.jpeg" alt="" class="img-fluid">
+                <div class="icon"><i class="fa fa-binoculars"></i></div>
+              </div>
+              <h2 class="title"><a href="#">Vision</a></h2>
+              <p>
+                To be the leading provider of Agile Performance Development solutions for 21st century workplace.
+              </p>
+            </div>
+          </div>
+
+          <div class="col-md-3 wow fadeInUp" data-wow-delay="0.1s">
+            <div class="about-col">
+              <div class="img">
+                <img src="<?php bloginfo('template_url'); ?>/img/mission.jpg" alt="" class="img-fluid">
+                <div class="icon"><i class="fa fa-rocket"></i></div>
+              </div>
+              <h2 class="title"><a href="#">Mission</a></h2>
+              <p>
+                To equip bussiness leaders with the tools to Drive Agile Performance Development in a 21st century context.
+
+            </div>
+          </div>
+
+          <div class="col-md-3 wow fadeInUp" data-wow-delay="0.2s">
+            <div class="about-col">
+              <div class="img">
+                <img src="<?php bloginfo('template_url'); ?>/img/values.jpg" alt="" class="img-fluid">
+                <div class="icon"><i class="fa fa-book"></i></div>
+              </div>
+              <h2 class="title"><a href="#">Values</a></h2>
+              <p>
+               We endeavor to lead authentically, innovatively, and effectively in all that we do. 
+              </p>
+            </div>
+          </div>
+
+           <div class="col-md-3 wow fadeInUp" data-wow-delay="0.2s">
+            <div class="about-col">
+              <div class="img">
+                <img src="<?php bloginfo('template_url'); ?>/img/purpose.jpg" alt="" class="img-fluid">
+                <div class="icon"><i class="fa fa-bullseye"></i></div>
+              </div>
+              <h2 class="title"><a href="#">Purpose</a></h2>
+              <p>
+               We want to bring about real change and make a real difference in organisational productivity across the world.
+              </p>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- #about -->
+          
+         
+       <!------------Form Markup Starts-------------> 
+       
+       <section class="contact-area" id="contact_area">
+           <div class="container-fluid">
+               <div class="row">
+                   <div class="col-md-6 col-md-offset-3 text-center">
+                       <div class="section-heading wow fadeInUp" data-wow-duration=".2s" data-wow-delay=".2s">
+                           <h2 class="section-title"><b>GET IN TOUCH</b></h2>
+                           <div class="border-bottom-now"></div>                           
+                       </div>                       
+                   </div>
+                 </div><br><br><br><br>
+                 <div class="row wow fadeInUp">
+                     <div class="col-md-8 col-md-offset-2">
+                         <form id="contactform" class="contact-form">
+                             <div class="row">
+                                <div class="col-md-6"><input id="name" type="text" class="form-control" name="name" placeholder="Name" required></div> 
+                                
+                                  <div class="col-md-6"><input id="email" class="form-control" type="email" name="Email" placeholder="Email" required></div> 
+                                 
+                                 <div class="col-md-12"><br>
+                                    <input type="text" id="subject" placeholder="Subject" class="form-control" name
+                                    ="name" required>
+                                  
+                                 </div>
+                                 <div class="col-md-12"><br>
+                                     <textarea id="message" rows="5" class="form-control" placeholder="Message" required></textarea>                                  
+                                 </div>
+                                 <div class="col-md-12 text-center"><br><br>
+                                     <button class="btn" type="submit">Submit Message</button>                                    
+                                 </div>
+                                
+                             </div>
+                            
+                         </form>
+                       
+                     </div>
+                     
+                 </div>
+                 <div class="row">                     
+                     <div class="contact-infos">
+                         <div class="col-md-4 text-center">
+                             <div class="contact-item wow fadeInLeft" data-wow-duration=".3s" data-wow-delay=".3s">
+                                 <span class="contact-icon"><i class="fa fa-map-marker"></i></span>
+                                 <p class="contact-detail">
+                                     P.O.Box 5505, Cresta 2118
+                             </div>                             
+                         </div>
+                            
+                              <div class="col-md-4 text-center">
+                             <div class="contact-item wow fadeInLeft" data-wow-duration=".6s" data-wow-delay=".6s">
+                                 <span class="contact-icon"><i class="fa fa-envelope"></i></span>
+                                 <p class="contact-detail">
+                                     info@deshundeysel.biz                                
+                                 </p>
+                             </div>                             
+                         </div> 
+                            
+                            
+                            <div class="col-md-4 text-center">
+                             <div class="contact-item wow fadeInLeft" data-wow-duration=".9s" data-wow-delay=".9s">
+                                 <span class="contact-icon"><i class="fa fa-phone"></i></span>
+                                 <p class="contact-detail">
+                                     +27 11 482-3996 - Central Office                                 
+                                 </p>
+                             </div>                             
+                         </div>                         
+                     </div>              
+                 </div>  
+           </div>        
+         </section>
+       <!-----------Markup for Maps----------------->
+    <?php get_footer(); ?>
+
+     
+
+     
